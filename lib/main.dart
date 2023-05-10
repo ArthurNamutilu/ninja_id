63f6242b6d8804baeb51dd8cf5a542e3326c3d7d
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-05-10 22:46:52 +0000

    Ninja ID

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,56 @@ class NinjaCard extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
       return Scaffold(
+        backgroundColor: Colors.grey[900],
         appBar: AppBar(
           title: Text('Ninja ID Card'),
+          centerTitle: true,
+          backgroundColor: Colors.grey[850],
+          elevation: 0.0,
+        ),
+        body: Padding(
+          padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'NAME',
+                style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0,
+                ),
+              ),
+              const SizedBox(height: 10.0), //space between name & Chun-Li
+              Text(
+                'Chun-Li',
+                style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              const SizedBox(height: 30.0),
+              const Text(
+                'CURRENT NINJA LEVEL',
+                style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0,
+                ),
+              ),
+              const SizedBox(height: 10.0), //space between name & Chun-Li
+              Text(
+                '8',
+                style: TextStyle(
+                    color: Colors.amberAccent[200],
+                    letterSpacing: 2.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              const SizedBox(height: 30.0),
+            ],
+          ),
         ),
       );
     }
